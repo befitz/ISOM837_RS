cbe19d7029c3dbe7fdf86e783a96ac1d127e0b20
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-12-07 01:07:08 -0500

    Create cat_map.py

---
+++ cat_map.py
@@ -0,0 +1,35 @@
+low_scale_seat_map = {'Grandstand':1, 'FieldBox':1, 'LogeBox':1, 'Bleachers':1, 'Pavilion':1,
+       'HP_Pavilion':0, 'HP_Dugout':0, 'Suites':0, 'RoofDeck':1, 'Barstools':1,
+       'DugoutBox':0, 'Terrace':1, 'Standing':1, 'GreenMonster':0, 'PressBooth':0, 'HP_FieldBox':1, 'Lawn': 1}
+
+med_scale_seat_map = {'Grandstand':0, 'FieldBox':0, 'LogeBox':0, 'Bleachers':0, 'Pavilion':0,
+       'HP_Pavilion':1, 'HP_Dugout':0, 'Suites':1, 'RoofDeck':0, 'Barstools':0,
+       'DugoutBox':0, 'Terrace':0, 'Standing':0, 'GreenMonster':1, 'PressBooth':1, 'HP_FieldBox':0, 'Lawn': 0}
+
+high_scale_seat_map = {'Grandstand':0, 'FieldBox':0, 'LogeBox':0, 'Bleachers':0, 'Pavilion':0,
+       'HP_Pavilion':0, 'HP_Dugout':1, 'Suites':0, 'RoofDeck':0, 'Barstools':0,
+       'DugoutBox':1, 'Terrace':0, 'Standing':0, 'GreenMonster':0, 'PressBooth':0, 'HP_FieldBox':0, 'Lawn': 0}
+
+SEASON_map = {'SEASON':1, 'SEABUS':0, 'GROUP':0, 'BUSINESS':0, 'IND':0, 'SPONSOR':0, 'EMPLOYEE':0}
+SEABUS_map = {'SEASON':0, 'SEABUS':1, 'GROUP':0, 'BUSINESS':0, 'IND':0, 'SPONSOR':0, 'EMPLOYEE':0}
+GROUP_map = {'SEASON':0, 'SEABUS':0, 'GROUP':1, 'BUSINESS':0, 'IND':0, 'SPONSOR':0, 'EMPLOYEE':0}
+BUSINESS_map = {'SEASON':0, 'SEABUS':0, 'GROUP':0, 'BUSINESS':1, 'IND':0, 'SPONSOR':0, 'EMPLOYEE':0}
+IND_map = {'SEASON':0, 'SEABUS':0, 'GROUP':0, 'BUSINESS':0, 'IND':1, 'SPONSOR':0, 'EMPLOYEE':0}
+SPONSOR_map = {'SEASON':0, 'SEABUS':0, 'GROUP':0, 'BUSINESS':0, 'IND':0, 'SPONSOR':1, 'EMPLOYEE':0}
+EMPLOYEE_map = {'SEASON':0, 'SEABUS':0, 'GROUP':0, 'BUSINESS':0, 'IND':0, 'SPONSOR':0, 'EMPLOYEE':1}
+
+April_map = {'March':0,'April':1, 'May':0, 'June':0, 'July':0, 'August':0, 'September':0, 'October':0}
+May_map = {'March':0,'April':0, 'May':1, 'June':0, 'July':0, 'August':0, 'September':0, 'October':0}
+June_map = {'March':0,'April':0, 'May':0, 'June':1, 'July':0, 'August':0, 'September':0, 'October':0}
+July_map = {'March':0,'April':0, 'May':0, 'June':0, 'July':1, 'August':0, 'September':0, 'October':0}
+August_map = {'March':0,'April':0, 'May':0, 'June':0, 'July':0, 'August':1, 'September':0, 'October':0}
+September_map = {'March':0,'April':0, 'May':0, 'June':0, 'July':0, 'August':0, 'September':1, 'October':0}
+
+
+Thurs_map = {'Thursday':1, 'Sunday':0, 'Tuesday':0, 'Wednesday':0, 'Saturday':0, 'Friday':0,'Monday':0}
+Sun_map = {'Thursday':0, 'Sunday':1, 'Tuesday':0, 'Wednesday':0, 'Saturday':0, 'Friday':0,'Monday':0}
+Tues_map = {'Thursday':0, 'Sunday':0, 'Tuesday':1, 'Wednesday':0, 'Saturday':0, 'Friday':0,'Monday':0}
+Weds_map = {'Thursday':0, 'Sunday':0, 'Tuesday':0, 'Wednesday':1, 'Saturday':0, 'Friday':0,'Monday':0}
+Sat_map = {'Thursday':0, 'Sunday':0, 'Tuesday':0, 'Wednesday':0, 'Saturday':1, 'Friday':0,'Monday':0}
+Fri_map = {'Thursday':0, 'Sunday':0, 'Tuesday':0, 'Wednesday':0, 'Saturday':0, 'Friday':1,'Monday':0}
+Mon_map = {'Thursday':0, 'Sunday':0, 'Tuesday':0, 'Wednesday':0, 'Saturday':0, 'Friday':0,'Monday':1}
