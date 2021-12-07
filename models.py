413714285759026c7120d4baa9af4e5b5196aa6c
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-12-07 01:07:14 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+from scipy import stats
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.tree import plot_tree
+import warnings
